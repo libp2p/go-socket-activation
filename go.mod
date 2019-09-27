156f67c5260a1c580dd9368f368eb95e2cc24ad3
Steven Allen <steven@stebalien.com>
2019-09-27 13:51:34 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/libp2p/go-socket-activation
+
+go 1.13
+
+require (
+	github.com/coreos/go-systemd v0.0.0-20190719114852-fd7a80b32e1f
+	github.com/ipfs/go-log v0.0.1
+	github.com/multiformats/go-multiaddr-net v0.1.0
+)
---
+++ sockets_test.go
@@ -0,0 +1,86 @@
+package sockets
+
+import (
+	"io/ioutil"
+	"net"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestActivation(t *testing.T) {
+	lt, err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.IPv6loopback})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c, err := lt.SyscallConn(); err == nil {
+		var fd uintptr
+		c.Control(func(nfd uintptr) {
+			fd = nfd
+		})
+		if fd != 3 {
+			t.Fatal("expected fd to be 3")
+		}
+	} else {
+		t.Fatal(err)
+	}
+
+	lu, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv6loopback})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c, err := lu.SyscallConn(); err == nil {
+		var fd uintptr
+		c.Control(func(nfd uintptr) {
+			fd = nfd
+		})
+		if fd != 4 {
+			t.Fatal("expected fd to be 3")
+		}
+	} else {
+		t.Fatal(err)
+	}
+
+	os.Setenv("LISTEN_FDS", "2")
+	os.Setenv("LISTEN_PID", strconv.Itoa(os.Getpid()))
+	os.Setenv("LISTEN_FDNAMES", "tcp:udp")
+	tcp, err := TakeListeners("tcp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tcp) != 1 {
+		t.Fatalf("expected 1 listener, got %d", len(tcp))
+	}
+
+	go func() {
+		l := tcp[0]
+		c, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		c.Write([]byte("foobar"))
+		c.Close()
+	}()
+	c, err := net.DialTCP("tcp", nil, lt.Addr().(*net.TCPAddr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	b, err := ioutil.ReadAll(c)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(b) != "foobar" {
+		t.Error("wrong message")
+	}
+	udp, err := TakePacketConns("udp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(udp) != 1 {
+		t.Fatalf("expected 1 listener, got %d", len(tcp))
+	}
+	if udp[0].LocalAddr().String() != lu.LocalAddr().String() {
+		t.Fatal("got the wrong udp listener")
+	}
+}
